083e2a2f4bea53cca8818b9f3f893669cfb3623d
David Roundy <roundyd@physics.oregonstate.edu>
2015-08-30 15:23:04 -0400

    add plural macros for splitting arrays

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ extern crate quickcheck;
 
 /// You can use `array_ref` to generate an array reference to a subset
 /// of a sliceable bit of data (which could be an array, or a slice,
-/// or a Vec).  The arguments are a bit clumsy
+/// or a Vec).
 #[macro_export]
 macro_rules! array_ref {
     ($arr:expr, $offset:expr, $len:expr) => {{
@@ -32,6 +32,60 @@ macro_rules! array_ref {
             let slice = & $arr[$offset..$offset+$len];
             unsafe {
                 as_array(slice)
+            }
+        }
+    }}
+}
+
+/// You can use `array_refs` to generate a series of array references
+/// to an input array reference.  The idea is if you want to break an
+/// array into a series of contiguous and non-overlapping arrays.
+#[macro_export]
+macro_rules! array_refs {
+    ( $arr:expr, $( $len:expr ),* ) => {{
+        {
+            #[inline]
+            #[allow(unused_assignments)]
+            unsafe fn as_arrays<T>(a: &[T; $( $len + )* 0 ]
+                                   ) -> ( $( &[T; $len], )* ) {
+                let mut p = a.as_ptr() as *const T;
+                ( $( {
+                    let aref = &*(p as *const [T; $len]);
+                    p = p.offset($len);
+                    aref
+                } ),* )
+            }
+            let input = $arr;
+            unsafe {
+                as_arrays(input)
+            }
+        }
+    }}
+}
+
+
+/// You can use `mut_array_refs` to generate a series of mutable array
+/// references to an input mutable array reference.  The idea is if
+/// you want to break an array into a series of contiguous and
+/// non-overlapping arrays.
+#[macro_export]
+macro_rules! mut_array_refs {
+    ( $arr:expr, $( $len:expr ),* ) => {{
+        {
+            #[inline]
+            #[allow(unused_assignments)]
+            unsafe fn as_arrays<T>(a: &mut[T; $( $len + )* 0 ]
+                                   ) -> ( $( &mut[T; $len], )* ) {
+                let mut p = a.as_ptr() as *mut T;
+                ( $( {
+                    let aref = &mut *(p as *mut [T; $len]);
+                    p = p.offset($len);
+                    aref
+                } ),* )
+            }
+            let input = $arr;
+            unsafe {
+                as_arrays(input)
             }
         }
     }}
@@ -135,4 +189,48 @@ fn check_array_mut_ref_out_of_bounds_32() {
         })
     }
     quickcheck::quickcheck(f as fn(Vec<u8>, usize) -> quickcheck::TestResult);
+}
+
+
+#[test]
+fn test_5_array_refs() {
+    let mut data: [usize; 128] = [0; 128];
+    for i in 0..128 {
+        data[i] = i;
+    }
+    let data = data;
+    let (a,b,c,d,e) = array_refs!(&data, 1, 14, 3, 100, 10);
+    assert_eq!(a.len(), 1 as usize);
+    assert_eq!(b.len(), 14 as usize);
+    assert_eq!(c.len(), 3 as usize);
+    assert_eq!(d.len(), 100 as usize);
+    assert_eq!(e.len(), 10 as usize);
+    assert_eq!(a, array_ref![data, 0, 1]);
+    assert_eq!(b, array_ref![data, 1, 14]);
+    assert_eq!(c, array_ref![data, 15, 3]);
+    assert_eq!(e, array_ref![data, 118, 10]);
+}
+
+
+#[test]
+fn test_5_mut_xarray_refs() {
+    let mut data: [usize; 128] = [0; 128];
+    {
+        // temporarily borrow the data to modify it.
+        let (a,b,c,d,e) = mut_array_refs!(&mut data, 1, 14, 3, 100, 10);
+        assert_eq!(a.len(), 1 as usize);
+        assert_eq!(b.len(), 14 as usize);
+        assert_eq!(c.len(), 3 as usize);
+        assert_eq!(d.len(), 100 as usize);
+        assert_eq!(e.len(), 10 as usize);
+        *a = [1; 1];
+        *b = [14; 14];
+        *c = [3; 3];
+        *d = [100; 100];
+        *e = [10; 10];
+    }
+    assert_eq!(&[1;1], array_ref![data, 0, 1]);
+    assert_eq!(&[14;14], array_ref![data, 1, 14]);
+    assert_eq!(&[3;3], array_ref![data, 15, 3]);
+    assert_eq!(&[10;10], array_ref![data, 118, 10]);
 }
